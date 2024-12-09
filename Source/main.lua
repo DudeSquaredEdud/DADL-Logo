662cbe7f1a66a80eb15664f8abf29daaddccfcee
AshtonAndrepont <C00462480@louisiana.edu>
2024-12-09 17:07:31 -0600

    Boxsterize!

--- Source/main.lua
+++ Source/main.lua
@@ -3,14 +3,8 @@ local moonshine = require 'moonshine'
 function love.load()
   love.window.setMode(1920, 1080)
 
-  backgroundEffect = moonshine(moonshine.effects.boxblur)
-  .chain(moonshine.effects.posterize)
-  .chain(moonshine.effects.scanlines)
-  .chain(moonshine.effects.pixelate)
-  backgroundEffect.pixelate.size = {3,3}
-  backgroundEffect.scanlines.thickness = .3
-  backgroundEffect.scanlines.opacity = .1
-  backgroundEffect.posterize.num_bands = 90
+  backgroundEffect = moonshine(moonshine.effects.boxsterize)
+  backgroundEffect.boxsterize.num_bands = 90
 
   scanlinesEffect = moonshine(moonshine.effects.scanlines)
 
@@ -33,8 +27,7 @@ end
 
 function love.update(dt)
   time = love.timer.getTime()
-  backgroundEffect.boxblur.radius = {(math.abs(math.sin(time*.1)*math.sin(time*.1)*700))+.1,1}
-  backgroundEffect.scanlines.phase = time*5
+  backgroundEffect.boxsterize.radius = {(math.abs(math.sin(time*.1)*math.sin(time*.1)*700))+.1,1}
 
   scanlinesEffect.scanlines.phase = time*10
 
